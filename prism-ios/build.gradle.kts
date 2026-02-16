46b58962f1eeed95bcab6e4cfe100d5191ab2ced
David Berrios <iamstuffed@gmail.com>
2026-02-16 02:24:54 -0800

    refactor: generate Prism.kt with KotlinPoet from gradle.properties
    - Replace hand-maintained Prism.kt with build-time generation using
      KotlinPoet, sourcing VERSION from gradle.properties VERSION_NAME
    - Remove static source file in favor of generated output under build/

--- prism-ios/build.gradle.kts
+++ prism-ios/build.gradle.kts
@@ -1,6 +1,44 @@
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
+buildscript {
+  repositories { mavenCentral() }
+  dependencies { classpath("com.squareup:kotlinpoet:2.2.0") }
+}
+
 plugins { alias(libs.plugins.kotlin.multiplatform) }
+
+val generatedDir = layout.buildDirectory.dir("generated/src/iosMain/kotlin")
+
+val generatePrismVersion by
+  tasks.registering {
+    val versionName = providers.gradleProperty("VERSION_NAME")
+    val outputDir = generatedDir
+    inputs.property("version", versionName)
+    outputs.dir(outputDir)
+    doLast {
+      val fileSpec =
+        FileSpec.builder("com.hyeonslab.prism", "Prism")
+          .addType(
+            TypeSpec.objectBuilder("Prism")
+              .addKdoc(
+                "Prism engine version information for the iOS XCFramework distribution."
+              )
+              .addProperty(
+                PropertySpec.builder("VERSION", String::class)
+                  .addModifiers(KModifier.CONST)
+                  .initializer("%S", versionName.get())
+                  .build()
+              )
+              .build()
+          )
+          .build()
+      fileSpec.writeTo(outputDir.get().asFile)
+    }
+  }
 
 kotlin {
   val xcf = XCFramework("Prism")
@@ -27,6 +65,7 @@ kotlin {
   applyDefaultHierarchyTemplate()
 
   sourceSets {
+    iosMain { kotlin.srcDir(generatePrismVersion) }
     commonMain.dependencies {
       api(project(":prism-math"))
       api(project(":prism-core"))
--- prism-ios/src/iosMain/kotlin/com/hyeonslab/prism/Prism.kt
+++
@@ -1,6 +0,0 @@
-package com.hyeonslab.prism
-
-/** Prism engine version information for the iOS XCFramework distribution. */
-object Prism {
-  const val VERSION = "0.1.0-SNAPSHOT"
-}
